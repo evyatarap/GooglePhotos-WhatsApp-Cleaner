1043b9f4b8be0c2bf2060e7ef2a0ef20a400ca71
Evyatar A. <evyatarap@gmail.com>
2024-08-20 23:41:22 +0300

    feat: Add logging for invalid media item IDs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import re
 from google.auth.transport.requests import Request
@@ -28,11 +29,22 @@ def split_into_batches(media_items, batch_size):
     for i in range(0, len(media_items), batch_size):
         yield media_items[i:i + batch_size]
 
+def is_valid_media_item_id(service, media_item_id):
+    try:
+        response = service.mediaItems().get(mediaItemId=media_item_id).execute()
+        return True
+    except Exception as e:
+        logging.error(f"Invalid media item ID {media_item_id}: {e}")
+        return False
+
 def add_media_items_to_album(service, album_id, media_item_ids):
     batch_size = 50
+   
+    is_valid_media_item_id(service, media_item_ids[0])
+
     for batch in split_into_batches(media_item_ids, batch_size):
         body = {
-            'mediaItemIds': batch
+            'mediaItemIds': [media_item_ids[23]]
         }
         response = service.albums().batchAddMediaItems(albumId=album_id, body=body).execute()
     return response
@@ -41,11 +53,49 @@ def is_valid_whatsapp_photo_or_video_filename(filename):
     pattern = r'^(IMG-\d{8}-WA\d{4}\.jpg|VID-\d{8}-WA\d{4}\.mp4)$'
     return re.match(pattern, filename) is not None
 
-def get_albums(service):
-    results = service.albums().list(
-        pageSize=50, fields="nextPageToken,albums(id,title)").execute()
-    items = results.get('albums', [])
-    return items
+def find_album_by_title(albums, title):
+    for album in albums:
+        if 'title' in album and album['title'] == title:
+            return album
+    return None
+
+def get_all_albums(service):
+    albums = []
+    nextPageToken = None
+    while True:
+        results = service.albums().list(
+            pageSize=50, fields="nextPageToken,albums(id,title)", pageToken=nextPageToken).execute()
+        albums += results.get('albums', [])
+        nextPageToken = results.get('nextPageToken', None)
+        if not nextPageToken:
+            break
+    return albums
+
+def get_last_added_media_item_in_album(service, album):
+    album_id = album['id']
+    media_items = []
+    nextPageToken = None
+
+    # Retrieve all media items in the album
+    while True:
+        response = service.mediaItems().search(body={'albumId': album_id, 'pageToken': nextPageToken}).execute()
+        media_items.extend(response.get('mediaItems', []))
+        nextPageToken = response.get('nextPageToken')
+        if not nextPageToken:
+            break
+
+    if not media_items:
+        return None
+
+    # Sort media items by creation time
+    media_items.sort(key=lambda item: item['mediaMetadata']['creationTime'])
+
+    # Get the last added media item
+    last_added_media_item = media_items[-1]
+    return last_added_media_item
+
+def get_media_item_datetime(media_item):
+    return datetime.strptime(media_item['mediaMetadata']['creationTime'], '%Y-%m-%dT%H:%M:%SZ')
 
 def get_photos_by_date(service, start_date, end_date, nextPageToken=None):
 
@@ -82,7 +132,6 @@ def get_photos_by_date(service, start_date, end_date, nextPageToken=None):
     return [items, results.get('nextPageToken', None)]
 
 def main():
-    """Shows basic usage of the Google Photos API."""
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -107,14 +156,24 @@ def main():
 
     service = build('photoslibrary', 'v1', credentials=creds, static_discovery=False)
 
-    # Call the Photos API
-
-    albums = get_albums(service)
+    last_item_date_time = None    
+    WA_MEDIA_ALBUM_TITLE = "WhatsApp-Media-Items"
+    DEFAULT_PERIOD_IN_DAYS = 100
     
+    existing_albums = get_all_albums(service)
+    wa_media_album = find_album_by_title(existing_albums, WA_MEDIA_ALBUM_TITLE)
+    if wa_media_album:
+        # Get the date and time of the media item that was the last to upload to Google Photos from a specific album
+        last_media_item = get_last_added_media_item_in_album(service, wa_media_album)
+        if last_media_item:
+            last_item_date_time = get_media_item_datetime(last_media_item)
+
     # Get a specific period of time
     end_date = datetime.now(timezone.utc)
-    start_date = end_date - timedelta(days=365*2)
-
+    if last_item_date_time:
+        start_date = last_item_date_time + timedelta(seconds=1)
+    else:
+        start_date = end_date - timedelta(days=DEFAULT_PERIOD_IN_DAYS)
 
     # Scan all media items between the start and end date and filter out WhatsApp media items
     whatsapp_media_items = []
@@ -126,23 +185,24 @@ def main():
         for photo in photos:
             if is_valid_whatsapp_photo_or_video_filename(photo['filename']):
                 whatsapp_media_items.append(photo)
-            #print('FileName: {0}'.format(photo['filename']))
         print("Scanned {0} photos, found {1} whatsApp.".format(total_media_items, len(whatsapp_media_items)))
         if not nextPageToken:
             break
     
     print('Found {0} WhatsApp photos from total {1} photos.'.format(len(whatsapp_media_items), total_media_items))
+        
 
-    # Create a new album
-    album_title = "WhatsApp-Media-Items"
-    wa_media_album = create_album(service, album_title)
-    print(f"Created album: {wa_media_album['title']} (ID: {wa_media_album['id']})")
+    # Create a new album for WhatsApp media items
+    if(not wa_media_album):
+        wa_media_album = create_album(service, WA_MEDIA_ALBUM_TITLE)
+        print(f"Created album: {wa_media_album['title']} (ID: {wa_media_album['id']})")
+    else:
+        print(f"Album already exists: {wa_media_album['title']} (ID: {wa_media_album['id']})")
 
     # Add WhatsApp media items to the new album
     media_item_ids = [media_item['id'] for media_item in whatsapp_media_items]
     add_media_items_to_album(service, wa_media_album['id'], media_item_ids)
-    
-
+    print(f"Added {len(whatsapp_media_items)} WhatsApp media items to the album.")
 
 
 if __name__ == '__main__':
